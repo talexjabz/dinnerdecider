2490d64aa1bcefa14c5db9ed65f26162b341c2c7
Ali Ziwa <ali.ziwa@hp.com>
2022-02-18 01:39:05 -0600

    Lab 1 logic

--- app/src/main/java/com/miu/mdp/labs/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/labs/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.miu.mdp.labs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.miu.mdp.labs.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
+    private val foodsList = arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.btnDecide.setOnClickListener {
+            val random = Random.nextInt(foodsList.size)
+            mainBinding.foodChoice.text = foodsList[random]
+        }
+
+        mainBinding.addFood.setOnClickListener {
+            val newFood = mainBinding.foodEditText.text.toString()
+
+            if (newFood.isEmpty()) return@setOnClickListener
+            foodsList.add(newFood)
+            mainBinding.foodEditText.clear()
+        }
     }
+}
+
+private fun EditText.clear() {
+    this.setText("")
 }
